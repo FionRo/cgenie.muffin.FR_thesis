a2264149de6b41a8b5f71a7ae9c7303a90613f8f
Fiona Rochholz <frochholz@marum.de>
2019-08-19 14:11:54 +0200

    first commit

---
+++ genie-userconfigs/DavidExDec18/StartDavid.sh
@@ -0,0 +1,58 @@
+cd User/frochholz/cgenie.muffin_hp_faster/genie-main
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 DDV20185.A1Aim2 10000 EXAMPLE.worjh2.Archeretal2009.SPIN2 &&
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 DDV201810.A1Aim2 10000 EXAMPLE.worjh2.Archeretal2009.SPIN2 &&
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 DDV201815.A1Aim2 10000 EXAMPLE.worjh2.Archeretal2009.SPIN2 &&
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 DDV201820.A1Aim2 10000 EXAMPLE.worjh2.Archeretal2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.Archer2009.SPIN1 10000
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 DDV201827SC.A1Aim2 10000 EXAMPLE.worjh2.Archeretal2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 ArcherA1Restart 6000 Archer2009.A1Aim
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES ArcherA1Restart02 6000 Archer2009.A1Aim2_02
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.Archer2009.A1Aim2 20000 worjh2.Archer2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.OrbitsDDV.A1Aim2 100000 worjh2.Archer2009.SPIN2
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.OrbitsDDV.A1Aim_1k 100000 worjh2.Archer2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.OrbitsDDV.A1Aim_+1k 100000 worjh2.Archer2009.SPIN2
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.OrbitsDDV.A1Aim_-20k 100000 worjh2.Archer2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim_-20k 100000 worjh2.Archer2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim_+2k 100000 worjh2.Archer2009.SPIN2
+
+scp /Users/frochholz/cgenie.muffin_hp_faster/genie-userconfigs/DavidExDec18/worjh2.OrbitsDDV.NG.A1Aim_-20k cluster7:/work/frochholz/cgenie.muffin/genie-userconfigs/DavidExDec18/worjh2.OrbitsDDV.NG.A1Aim_-20k
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.OrbitsDDV.NG.A1Aim_-20k 100000 worjh2.Archer2009.SPIN2
+
+scp /Users/frochholz/cgenie.muffin_hp_faster/genie-userconfigs/DavidExDec18/worjh2.OrbitsDDV.NG.A1Aim_+2k cluster7:/work/frochholz/cgenie.muffin/genie-userconfigs/DavidExDec18/worjh2.OrbitsDDV.NG.A1Aim_+2k
+
+scp /Users/frochholz/cgenie.muffin_hp_faster/genie-forcings/pyyyyz.FpCO2_Fp13CO2.A1_AIMOrbits+2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.OrbitsDDV.NG.A1Aim_+2k 100000 worjh2.Archer2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2 100000 worjh2.Archer2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2OrbitsNF 100000 worjh2.Archer2009.SPIN2
+
+
+
+ Auf Cluster7 am 01.03
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2+20kOrbits 100000 worjh2.Archer2009.SPIN2    - done
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2+4kOrbits 100000 worjh2.Archer2009.SPIN2   - done
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2-10kOrbits 100000 worjh2.Archer2009.SPIN2    - done
+
+still running
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2-20kOrbits 100000 worjh2.Archer2009.SPIN2
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2+10kOrbits 100000 worjh2.Archer2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg_gl.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2OrbitDetails 100000 worjh2.Archer2009.SPIN2
+
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg.worjh2.BASES DavidExDec18 worjh2.DDV.A1Aim2OrbitDetailsslow 100000 worjh2.Archer2009.SPIN2
+
+On Guest PC: (15.März)
+./runmuffin.sh cgenie.eb_go_gs_ac_bg_sg_rg.worjh2.BASES ModernForcing worjh2.DDV.A1Aim2+10kOrbitDetails 100000 worjh2.Archer2009.SPIN2
